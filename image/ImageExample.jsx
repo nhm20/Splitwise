e8adf8c86c1b8e8cf2324bf652b70ce4b2ab0752
nmo20 <nmo212182@gmail.com>
2025-08-19 21:03:20 +0530

    ScrollView

--- image/ImageExample.jsx
+++ image/ImageExample.jsx
@@ -1,29 +1,95 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import favicon from "../assets/favicon.png";
+import { ScrollView } from "react-native";
 const ImageExample = () => {
   return (
-    <View>
-      <Image
-        style={{
-          width: 200,
-          height: 200,
-        }}
-        source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg",
-        }}
-              loadingIndicatorSource={favicon}
-              resizeMode="center"
-      />
+    <ScrollView>
+      <View>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg",
+          }}
+          loadingIndicatorSource={favicon}
+          resizeMode="center"
+        />
 
-      <Image
-        style={{
-          width: 200,
-          height: 200,
-        }}
-        source={favicon}
-      />
-    </View>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={favicon}
+        />
+      </View>
+      <View>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg",
+          }}
+          loadingIndicatorSource={favicon}
+          resizeMode="center"
+        />
+
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={favicon}
+        />
+      </View>
+      <View>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg",
+          }}
+          loadingIndicatorSource={favicon}
+          resizeMode="center"
+        />
+
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={favicon}
+        />
+      </View>
+      <View>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg",
+          }}
+          loadingIndicatorSource={favicon}
+          resizeMode="center"
+        />
+
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={favicon}
+        />
+      </View>
+    </ScrollView>
   );
 };
 

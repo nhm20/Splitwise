53a22fd13f917e0bb5f386b1c4803b85540d966c
nmo20 <nmo212182@gmail.com>
2025-08-20 20:58:28 +0530

    Statusbar

--- App.jsx
+++ App.jsx
@@ -1,11 +1,26 @@
-import { StatusBar, Text, View } from "react-native";
-import ModalExample from "./ModalExample";
+import { useState } from "react";
+import { Button, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
   return (
-    <View style={{ width: "100%" }}>
-      <ModalExample />
-      <Text>Hello from App!</Text>
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor={"blue"}
+        animated={true}
+        hidden={!visible}
+        barStyle="dark-content"
+      />
+      <Button title="Toggle StatusBar" onPress={() => setVisible(!visible)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: StatusBar.currentHeight + 6,
+  },
+});

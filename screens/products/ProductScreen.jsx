21f49894c1643e0b322e37113a493a2b57aa817d
nmo20 <nmo212182@gmail.com>
2025-08-25 00:22:53 +0530

    Pull to Refresh

--- screens/products/ProductScreen.jsx
+++ screens/products/ProductScreen.jsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, RefreshControl, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import ProductItem from "../../components/ProductItem";
 import { useEffect } from "react";
@@ -7,9 +7,20 @@ const ProductScreen = () => {
   const products = useSelector(state => state.products.products)
   console.log(products);
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefreshList = () => {
+    setRefreshing(true);
+    dispatch(FETCH_PRODUCTS_THUNK()).finally(() => {
+      setRefreshing(false);
+    });
+  };
+
   useEffect(() => {
     dispatch(FETCH_PRODUCTS_THUNK());
   }, [dispatch]);
+
+
 
   return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
@@ -19,6 +30,9 @@ const ProductScreen = () => {
           <ProductItem {...item.item} />
         )}
         keyExtractor={item => item.id}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefreshList} />
+        }
       />
     </View>
   );

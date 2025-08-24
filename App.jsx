3035b9a6e5b48971dc9a59733334a3af8d90fadc
nmo20 <nmo212182@gmail.com>
2025-08-24 14:04:01 +0530

    Drawer Navigation

--- App.jsx
+++ App.jsx
@@ -1,7 +1,28 @@
-import Navigation from './navigation/Index.jsx'
+import 'react-native-gesture-handler'
+import {
+  createDrawerNavigator,
+  DrawerToggleButton,
+} from "@react-navigation/drawer";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import User from "./screens/User";
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <Navigation />
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerLeft: () => <DrawerToggleButton />,
+        }}
+      >
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Profile" component={Profile} />
+        <Drawer.Screen name="User" component={User} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }

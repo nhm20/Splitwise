1c21a2ddbd5fd8a9458609cbfd0497786563c7b7
nmo20 <nmo212182@gmail.com>
2025-08-24 17:13:53 +0530

    Context Api

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler'
+import "react-native-gesture-handler";
 import {
   createDrawerNavigator,
   DrawerToggleButton,
@@ -6,23 +6,25 @@ import {
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import User from "./screens/User";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import CounterProvider from "./context/CounterContext";
 
 const Drawer = createDrawerNavigator();
 
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          headerLeft: () => <DrawerToggleButton />,
-        }}
-      >
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Profile" component={Profile} />
-        <Drawer.Screen name="User" component={User} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <CounterProvider>
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerLeft: () => <DrawerToggleButton />,
+          }}
+        >
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Profile" component={Profile} />
+          <Drawer.Screen name="User" component={User} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </CounterProvider>
   );
 }
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import { Button, Text, View } from "react-native";
+import { CounterContext } from "../context/CounterContext";
 
 const Home = () => {
+  const counter = useContext(CounterContext);
   return (
     <View>
       <Text>Home Screen</Text>
       <Text>Welcome to the home screen!</Text>
-      
+      <Text>Current Count: {counter.count}</Text>
+      <Button title="Increment" onPress={counter.increment} />
+      <Button title="Decrement" onPress={counter.decrement} />
+      <Button title="Reset" onPress={counter.reset} />
     </View>
   );
 };
